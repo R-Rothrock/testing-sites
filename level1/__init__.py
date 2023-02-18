751972776d88d0218c19cb203851edfcd4803d72
Roan Rothrock <roan.rothrock@hotmail.com>
2023-02-18 15:43:13 -0600

    changed directory structure

--- level1/__init__.py
+++ level1/__init__.py
@@ -11,12 +11,12 @@ def index():
         user_in = request.form["in"]
     else:
         user_in = "Say hey to the form!"
-    with open("index.html", "r") as stream:
+    with open("./app/index.html", "r") as stream:
         return render_template_string(stream.read() % (user_in))
 
 @app.route("/index.js")
-def inde_js():
-    with open("./index.js", "r") as stream:
+def index_js():
+    with open("./app/index.js", "r") as stream:
         return stream.read()
 
 if __name__ == "__main__":
