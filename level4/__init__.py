c1ec64ea9fa3309fc3a4a65f8909cb43c1cb1e50
Roan Rothrock <roan.rothrock@hotmail.com>
2023-03-04 12:16:27 -0600

    deving. `level4` still doesn't work

--- level4/__init__.py
+++ level4/__init__.py
@@ -2,11 +2,48 @@
 
 # If you have a FileNotFoundError running this script,
 # you need to install `host` which is used for DNS and rDNS.
+# Debian/Ubuntu/Mint/Kali/Parrot/Kubuntu/many more:
+# ~$ sudo apt install dnsutils
+# Arch/Manjaro
+# ~$ sudo pacman -S bind
 
 from flask import Flask, render_template, request
 import subprocess
+import time
+import threading
 
 dns_ret = "waiting for input"
+
+def get_output(cmd: str, shell="/bin/bash") -> str:
+    """
+    This code is horrible. But it should work for you :)
+    I programmed this on a Saturday morning, don't blame me.
+    """
+
+    cmd = cmd.encode("utf-8")
+
+    process = subprocess.Popen(shell, stdout=subprocess.PIPE,
+                               stdin=subprocess.PIPE)
+
+    process.stdin.write(cmd)
+    process.stdin.flush()
+
+    ret = str()
+
+    activator = False
+
+    def timer(sec: int):
+        """ Small threading timer functionality. Waits for sleep """
+        nonlocal activator
+        time.sleep(sec)
+        activator = True
+
+    threading.Thread(target=timer, args=(5,)).start()
+
+    while not activator:
+        ret += process.stdout.read(1)
+
+    return ret
 
 app = Flask(__name__)
 
@@ -15,7 +52,7 @@ def index():
     if request.method == "POST":
         user_in = request.form["submit"]
         global dns_ret
-        dns_ret = subprocess.check_output(f"host {user_in}".split(" "))
+        dns_ret = get_output(f"host {user_in}")
         print(dns_ret)
     return render_template("index.html", dns_ret=dns_ret)
 
