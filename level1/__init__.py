4e28aebc8f0f8f906ee8e5104f1caec4b64004bc
Roan Rothrock <roan.rothrock@hotmail.com>
2023-03-09 14:29:18 -0600

    Git CLI had stroke; undid latest commit

---
+++ level1/__init__.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+# app.py
+
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        user_in = request.form["in"]
+    else:
+        user_in = "Say hey to the form!"
+    with open("./templates/index.html", "r") as stream:
+        return stream.read() % user_in
+
+@app.route("/index.js")
+def index_js():
+    with open("./templates/index.js", "r") as stream:
+        return stream.read()
+
+if __name__ == "__main__":
+    app.run(host="localhost", port=5001, debug=False)
