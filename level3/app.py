45d4e2370dc8d9d4d25507c95fd7d3fd6f117091
Roan Rothrock <roan.rothrock@hotmail.com>
2023-03-14 09:38:38 -0500

    Added stuff to `level3`

--- level3/app.py
+++ level3/app.py
@@ -8,5 +8,9 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/robots.txt")
+def robots():
+    return render_template("robots.txt")
+
 if __name__ == "__main__":
     app.run(host="localhost", port=5001, debug=False)
