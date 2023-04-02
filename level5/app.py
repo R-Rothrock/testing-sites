23436a6b460f982c0c3e5e03e3de8739aa93b621
Roan Rothrock <roan.rothrock@hotmail.com>
2023-04-02 09:40:44 +0000

    Finished `level5`

--- level5/app.py
+++ level5/app.py
@@ -1,28 +1,39 @@
 #!/usr/bin/python3
 
-from flask import Flask, render_template, redirect, request
+from flask import Flask, render_template, redirect, Response, request
+
 import sqlite3
 
 connection = sqlite3.connect("./templates/sqlite.db", check_same_thread=False)
 cursor = connection.cursor()
 
-authenticated  = False
+def authenticate(password: str) -> int:
+    global cursor
+    command = f"SELECT true \
+              FROM Users \
+              WHERE username=\"admin\" \
+              AND password=\"{password}\";"
+    try:
+        ret = cursor.execute(command).fetchone()[0]
+    except TypeError:
+        return -1
+    if ret == 1:
+        return 0
+    return 1
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
-        password = request.form['login']
-        global authenticated
-        authenticated  = cursor.execute(
-            f"SELECT true FROM Users" +
-            " WHERE username=\"admin\"" +
-            " AND password=\"{password}\";"
-        ).fetchone()
-    if authenticated:
-        return redirect("/admin.html", code=302)
+        password = request.form.get("login")
+        match authenticate(password):
+            case 0:
+                return redirect("/admin", code=302)
+            case -1:
+                return "Internal Server Error", 500
     return render_template("admin_login.html")
+
 
 @app.route("/admin")
 def admin():
